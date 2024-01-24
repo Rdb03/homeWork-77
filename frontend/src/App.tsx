20d4d04c3335b99dbdb113eb44c2e207825adf79
Djaparov Ramil <rdb03@mail.ru>
2024-01-24 19:41:43 +0600

    Настроил проект для начала работы скачав нужные библиотеки.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import ToolBar from "./components/ToolBar/ToolBar.tsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => (
+    <>
+        <header>
+            <ToolBar/>
+        </header>
+        <main>
+
+        </main>
+    </>
+);
+
+export default App
