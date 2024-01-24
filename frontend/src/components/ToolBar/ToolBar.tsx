afbe339c2ca3c85778caf36c9ab4cfeba81ee844
Djaparov Ramil <rdb03@mail.ru>
2024-01-24 19:40:28 +0600

    Настроил проект для начала работы скачав нужные библиотеки.

---
+++ frontend/src/components/ToolBar/ToolBar.tsx
@@ -0,0 +1,11 @@
+import './ToolBar.css';
+
+const ToolBar = () => {
+    return (
+        <div className="toolBar">
+            <h1 className="toolBar-title">Guest Book</h1>
+        </div>
+    );
+};
+
+export default ToolBar;
